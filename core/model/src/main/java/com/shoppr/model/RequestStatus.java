a7ae783badfb48079c3227dcbc1bab9266bd280f
omarshatani <omar.shatani@gmail.com>
2025-10-02 23:40:29 +0200

    SHOPPR-68 removed comments

--- core/model/src/main/java/com/shoppr/model/RequestStatus.java
+++ core/model/src/main/java/com/shoppr/model/RequestStatus.java
@@ -3,8 +3,8 @@ package com.shoppr.model;
 public enum RequestStatus {
 	SELLER_PENDING,
 	BUYER_PENDING,
-	SELLER_ACCEPTED, // New: Buyer has accepted, awaiting seller's final confirmation
-	BUYER_ACCEPTED,  // New: Seller has accepted, awaiting buyer's final confirmation to pay
+	SELLER_ACCEPTED,
+	BUYER_ACCEPTED,
 	COMPLETED,
 	REJECTED
 }
